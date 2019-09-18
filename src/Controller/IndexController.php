7c94b63df7f774376d19ecedf2c13b976687f50a
Samolberg <l.samolberg@khk.ee>
2019-09-18 11:34:46 +0300

    First page

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+// src/Controller/LuckyController.php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class IndexController extends AbstractController
+{
+    public function index()
+    {
+        return new Response(
+            '<html><body><h1>Booking App index page</h1></body></html>'
+        );
+    }
+}
